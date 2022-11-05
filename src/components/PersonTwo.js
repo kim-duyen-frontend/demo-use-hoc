30491af913406bfb4ed1ce263276c69a0d1ca94a
kim-duyen-frontend <ntduyen255@gmail.com>
2022-11-05 15:31:17 +0700

    add code component Person 1 and Person 2

--- src/components/PersonTwo.js
+++ src/components/PersonTwo.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function PersonTwo(props) {
+function PersonTwo() {
+    const [number, setNumber] = useState(10);
+
+    const handleIncrementMoney = () => {
+        setNumber((prev) => prev * 2);
+    }
     return (
         <div>
-            
+            <h1>Hello Jimmy</h1>
+            <p>
+                <span>${number}</span> &nbsp;
+                <button onClick={handleIncrementMoney}>Increment</button>
+            </p>
         </div>
     );
 }
